5790c9cd1acf09976e18dfa4fc2e0795aa2f6cb9
mitra.patra16@gmail.com <mitra.patra16@gmail.com>
2022-08-25 12:38:07 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReadWrightIni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INIFile settingsIni = new INIFile (@".\\Testdata.ini");
+ 
+            string executionMode = settingsIni.Read ("BrowserName", "BrowserInfo").ToLower ();
+            Console.WriteLine (executionMode);
+ 
+            string WebdriverToUse = settingsIni.Read ("WebDriverLocation", "BrowserInfo").ToLower ();
+            Console.WriteLine (WebdriverToUse);
+            Console.WriteLine("Hello World!");
+
+            settingsIni.Write("TimeOut","4000","BrowserInfo");
+
+            string WebdriverToTimeOut = settingsIni.Read ("TimeOut", "BrowserInfo");
+            Console.WriteLine (WebdriverToTimeOut);
+
+            settingsIni.Write("BrowserName","IE","BrowserInfo");
+
+            settingsIni.Write("URL","https://www.google.co.in/","AppUnderTest");
+
+
+        }
+    }
+}
